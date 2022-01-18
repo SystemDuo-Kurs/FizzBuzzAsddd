ccd17f9aa5c6b5a18c2bd7f424c9c39d9bd357ed
Dragan Pepic <nedimco2006@gmail.com>
2022-01-18 17:07:52 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿do
+{
+    int maxBroj;
+    do
+    {
+        Console.Write("Unesite broj (od 5 navise): ");
+        maxBroj = int.Parse(Console.ReadLine());
+        if (maxBroj < 5)
+        {
+            Console.WriteLine("Jok!");
+        }
+        else
+        {
+            break;
+        }
+    } while (true);
+
+    for (int brojac = 1; brojac <= maxBroj; brojac++)
+    {
+        Console.Write(brojac);
+
+        if (brojac % 3 == 0)
+        {
+            Console.Write(" Fizz ");
+        }
+
+        if (brojac % 5 == 0)
+        {
+            Console.Write(" Buzz ");
+        }
+
+        Console.WriteLine();
+    }
+} while (true);
